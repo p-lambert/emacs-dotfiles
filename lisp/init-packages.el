3c09000a5bee676c7a72121ccef6a558386b5d61
Pedro Lambert <pedrolambert@gmail.com>
2015-03-24 01:55:15 -0300

    Ensure packages installation

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -4,5 +4,25 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
+(defvar my-packages
+  '(
+    helm
+    flatland-theme
+    magit
+    projectile
+    )
+  "A list of packages to be installed at application launch.")
+
+;; package loading (stolen from milhouse)
+(setq packaged-contents-refreshed-p nil)
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (condition-case ex
+  (package-install p)
+      ('error (if packaged-contents-refreshed-p
+      (error ex)
+    (package-refresh-contents)
+    (setq packaged-contents-refreshed-p t)
+    (package-install p))))))
 
 (provide 'init-packages)
