59dcb49abe83242b5155acc5f6a5e510013e38aa
Pedro Lambert <pedrolambert@gmail.com>
2015-03-24 14:34:42 -0300

    Improve Helm configuration

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -4,4 +4,7 @@
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
+(setq helm-quick-update t
+      helm-buffers-fuzzy-matching t)
+
 (provide 'init-helm)
