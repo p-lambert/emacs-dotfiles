8a0ec3284c0421628be86f4fe09e3a797453c5db
Pedro Lambert <pedrolambert@gmail.com>
2015-03-31 11:55:35 -0300

    Remove PATH update script from rbenv since it's no longer necessary

--- lisp/init-rbenv.el
+++ lisp/init-rbenv.el
@@ -1,10 +1,5 @@
 (require 'rbenv)
 
-;; add rbenv shims directory to PATH
-(let ((shims-dir (concat rbenv-installation-dir "shims"))
-      (home-dir (getenv "PATH")))
-      (setenv "PATH" (concat shims-dir ":" home-dir)))
-
 (add-hook 'ruby-mode-hook 'rbenv-use-corresponding)
 (add-hook 'eshell-mode-hook 'rbenv-use-corresponding)
 
