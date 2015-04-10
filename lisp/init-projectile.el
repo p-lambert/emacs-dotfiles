c4ca2b78405ea15769abdab5a35bebe8b4666a65
Pedro Lambert <pedrolambert@gmail.com>
2015-04-10 00:29:21 -0300

    Change projectile-switch-project-action

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -7,7 +7,7 @@
 
 (setq projectile-completion-system 'helm)
 
-(setq projectile-switch-project-action 'helm-projectile-find-file)
+(setq projectile-switch-project-action 'projectile-dired)
 
 (setq helm-projectile-sources-list '(helm-source-projectile-buffers-list
                                      helm-source-projectile-files-list))
