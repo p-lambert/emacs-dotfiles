41dee5794922a4533d90233f67921d7cc6c5ac94
Pedro Lambert <pedrolambert@gmail.com>
2015-04-16 14:07:53 -0300

    Improve smartparens configurations

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -1,12 +1,22 @@
-(require 'smartparens)
 (require 'smartparens-config)
 
 (smartparens-global-mode t)
 
 ;; kill sexp
-(define-key sp-keymap (kbd "C-M-k") 'sp-kill-hybrid-sexp)
+(define-key sp-keymap (kbd "C-M-k") 'sp-kill-sexp)
+
+;; sexp movements
+(define-key sp-keymap (kbd "C-M-f") 'sp-forward-sexp)
+(define-key sp-keymap (kbd "C-M-b") 'sp-backward-sexp)
 
 (sp-with-modes '(ruby-mode)
   (sp-local-pair "|" "|"))
+
+;; parens display
+(setq sp-show-pair-delay 0)
+(setq sp-show-pair-from-inside t)
+
+(add-hook 'lisp-mode-hook 'show-smartparens-mode)
+(add-hook 'emacs-lisp-mode-hook 'show-smartparens-mode)
 
 (provide 'init-smartparens)
