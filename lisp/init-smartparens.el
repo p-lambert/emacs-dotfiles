129d7c5c8947379fbc85264c87814532ffbb09cf
Pedro Lambert <pedrolambert@gmail.com>
2015-04-19 13:30:10 -0300

    Disable annoying smartparens color overlay

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -16,6 +16,8 @@
 (setq sp-show-pair-delay 0)
 (setq sp-show-pair-from-inside t)
 
+(set-face-attribute 'sp-pair-overlay-face nil :inherit nil)
+
 (add-hook 'lisp-mode-hook 'show-smartparens-mode)
 (add-hook 'emacs-lisp-mode-hook 'show-smartparens-mode)
 
