debca03deb8db493c7d66c5e0ef64635c7a4f0cf
Pedro Lambert <pedrolambert@gmail.com>
2015-04-23 16:24:23 -0300

    Add indentation fixes to ruby-mode

--- lisp/init-ruby.el
+++ lisp/init-ruby.el
@@ -28,6 +28,30 @@
 ;; enable debugging tools (binding.pry or byebug)
 (add-hook 'after-init-hook 'inf-ruby-switch-setup)
 
+;; identation fixes
+(setq
+ ruby-align-chained-calls nil
+ ruby-align-to-stmt-keywords nil
+ ruby-deep-indent-paren nil
+ ruby-deep-indent-paren-style nil
+ ruby-use-smie nil)
+
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  "Ensure desired behaviour for parenthesis indentation."
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
 ;; vcr toggle
 (defun custom/vcr-toggle ()
   (interactive)
