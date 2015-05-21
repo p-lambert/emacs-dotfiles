0491d8aebc2e581fd6ed75d6ccb994d4404452ae
Pedro Lambert <pedrolambert@gmail.com>
2015-05-21 18:05:56 -0300

    Change magit's keybindings

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -1,6 +1,8 @@
 (require 'magit)
 
-(global-set-key (kbd "C-c g") 'magit-status)
+(global-set-key (kbd "C-c g g") 'magit-status)
+(global-set-key (kbd "C-c g c") 'magit-checkout)
+(global-set-key (kbd "C-c g b") 'magit-branch-manager)
 
 ;; disable magit startup message
 (setq magit-last-seen-setup-instructions "1.4.0")
