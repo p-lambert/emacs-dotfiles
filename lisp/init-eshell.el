e9557238707c196f3b4d20754cb6e0abff6582e4
Pedro Lambert <pedrolambert@gmail.com>
2015-06-17 20:14:54 -0300

    Add function for executing commands through bash inside eshell

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -2,4 +2,11 @@
 
 (setq eshell-aliases-file (concat user-emacs-directory ".eshell-aliases"))
 
+(defun custom/bash-command (&rest cmd)
+  "Run CMD as if you were in a bash shell instead of Eshell."
+  (insert (format "bash -c 'source ~/.bashrc; cd %s; %s'"
+                  (eshell/pwd)
+                  (mapconcat 'identity (car cmd) " ")))
+  (eshell-send-input))
+
 (provide 'init-eshell)
