bb17b12b1a56de4be01f493f7602c2dde3090aef
Pedro Lambert <pedrolambert@gmail.com>
2015-06-23 13:25:47 -0300

    Improve web-mode configuration

--- lisp/init-general.el
+++ lisp/init-general.el
@@ -24,7 +24,7 @@
 (global-whitespace-mode)
 
 (setq whitespace-global-modes
-      '(not magit-mode git-commit-mode dired-mode))
+      '(not magit-mode git-commit-mode dired-mode web-mode))
 
 (setq whitespace-style
       '(
--- lisp/init-web.el
+++ lisp/init-web.el
@@ -1,6 +1,7 @@
 (require 'web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 ;; see web-mode-current-element-highlight-face
 (setq web-mode-enable-current-element-highlight t)
