41bcd39270445b87357b2def5a9b5cb92a592eb1
Pedro Lambert <pedrolambert@gmail.com>
2015-06-24 16:24:16 -0300

    Add keybinding for opening git blame

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -5,6 +5,7 @@
 (global-set-key (kbd "C-c g c") 'magit-checkout)
 (global-set-key (kbd "C-c g b") 'magit-branch-manager)
 (global-set-key (kbd "C-c g t") 'git-timemachine)
+(global-set-key (kbd "C-c g l") 'vc-annotate)
 
 ;; disable magit startup message
 (setq magit-last-seen-setup-instructions "1.4.0")
