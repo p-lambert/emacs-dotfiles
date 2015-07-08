05f3f82cf0dc640af88f6f3889925cfc30c25507
Pedro Lambert <pedrolambert@gmail.com>
2015-07-08 12:39:12 -0300

    Always open `guide-key` at the bottom of the screen

--- lisp/init-guide-key.el
+++ lisp/init-guide-key.el
@@ -11,4 +11,7 @@
 ;; check keys recursively
 (setq guide-key/recursive-key-sequence-flag t)
 
+;; always open a horizontal split at the bottom
+(setq guide-key/popup-window-position 'bottom)
+
 (provide 'init-guide-key)
