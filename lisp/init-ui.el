f5b437f85a564169434f43b2789c0287af4aae4e
Pedro Lambert <pedrolambert@gmail.com>
2015-09-21 09:05:18 -0400

    Improve line number display

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -28,7 +28,10 @@
 (load-theme 'jellyburn t)
 
 ;; linum
-(setq linum-format "%3d")
+(defadvice linum-update-window (around linum-dynamic activate)
+  (let* ((w (length (number-to-string (count-lines (point-min) (point-max)))))
+         (linum-format (concat "%" (number-to-string w) "d ")))
+    ad-do-it))
 
 ;; modeline
 (defun branch-name ()
