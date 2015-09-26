ef090f21d4b6e137a513f4ed956b771327b524fc
Pedro Lambert <pedrolambert@gmail.com>
2015-09-26 08:59:04 -0400

    Give line numbers one whitespace of left margin

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -30,7 +30,7 @@
 ;; linum
 (defadvice linum-update-window (around linum-dynamic activate)
   (let* ((w (length (number-to-string (count-lines (point-min) (point-max)))))
-         (linum-format (concat "%" (number-to-string w) "d ")))
+         (linum-format (concat " %" (number-to-string w) "d ")))
     ad-do-it))
 
 ;; modeline
