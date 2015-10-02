8eb2d007ce2852c8cff0142d5cab3c6daeb4b1eb
Pedro Lambert <pedrolambert@gmail.com>
2015-10-02 15:18:56 -0400

    Enable `custom/smart-move...` command for multiple-cursors

--- lisp/init-multiple-cursors.el
+++ lisp/init-multiple-cursors.el
@@ -5,4 +5,8 @@
 (global-set-key (kbd "C-c m a") 'mc/mark-all-like-this)
 (global-set-key (kbd "C-c m r") 'mc/set-rectangular-region-anchor)
 
+;; commands to run for all cursors
+(setq mc/cmds-to-run-for-all
+      '(custom/smart-move-beginning-of-line))
+
 (provide 'init-multiple-cursors)
