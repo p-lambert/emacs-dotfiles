5d795ca6bde9aa2fdbd200043886eb872c50b570
Pedro Lambert <pedrolambert@gmail.com>
2015-10-30 10:42:34 -0400

    Add web-mode setup

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -6,4 +6,10 @@
 ;; see web-mode-current-element-highlight-face
 (setq web-mode-enable-current-element-highlight t)
 
+(defun custom/web-mode-setup ()
+  (setq web-mode-markup-indent-offset 4)
+  (toggle-truncate-lines 1))
+
+(add-hook 'web-mode-hook 'custom/web-mode-setup)
+
 (provide 'init-web)
