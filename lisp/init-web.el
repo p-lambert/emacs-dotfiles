97d752f6e154296a9a69cf5b9e43aa69b5494de1
Pedro Lambert <pedrolambert@gmail.com>
2015-10-30 11:12:43 -0400

    Add `custom/prompt-web-mode-indent`

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -6,8 +6,18 @@
 ;; see web-mode-current-element-highlight-face
 (setq web-mode-enable-current-element-highlight t)
 
+(defvar custom/web-mode-indent 4
+  "Number of spaces used by web-mode to indent code.")
+
+(defun custom/prompt-web-mode-indent ()
+  (interactive)
+  (let* ((default (number-to-string custom/web-mode-indent))
+         (indent-offset
+          (read-string "Number of spaces to indent: " default nil default)))
+    (setq web-mode-markup-indent-offset (string-to-number indent-offset))))
+
 (defun custom/web-mode-setup ()
-  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-markup-indent-offset custom/web-mode-indent)
   (toggle-truncate-lines 1))
 
 (add-hook 'web-mode-hook 'custom/web-mode-setup)
