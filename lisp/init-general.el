87acaf0efa4f622f73add7368b275cb38c6f463b
Pedro Lambert <pedrolambert@gmail.com>
2016-02-29 18:58:59 -0500

    Allow toggling whitespace removal on `before-save-hook`

--- lisp/init-general.el
+++ lisp/init-general.el
@@ -35,7 +35,16 @@
         lines-tail
         ))
 
+(defun custom/toggle-whitespace-removal ()
+  (interactive)
+  (if (member 'delete-trailing-whitespace before-save-hook)
+      (progn
+        (remove-hook 'before-save-hook 'delete-trailing-whitespace)
+        (message "Remove trailing whitespace DISABLED."))
+    (add-hook 'before-save-hook 'delete-trailing-whitespace)
+    (message "Remove trailing whitespace ENABLED.")))
+
 ;; automatic whitespace removal
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(custom/toggle-whitespace-removal)
 
 (provide 'init-general)
