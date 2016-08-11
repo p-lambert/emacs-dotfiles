9810a4f0bd731cf85491d8e4696c1dcf9eb9742b
Pedro Lambert <pedrolambert@gmail.com>
2016-08-11 12:17:29 -0400

    Clean `init-markdown`

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -4,9 +4,7 @@
 ;; disable autoindent after pressing RET
 (setq markdown-indent-on-enter nil)
 
-;; enable visual-fill-column for markdown files
-(add-hook 'markdown-mode-hook
-          (lambda () (yas-minor-mode)))
+(add-hook 'markdown-mode-hook (lambda () (yas-minor-mode)))
 
 ;; use vanilla yank function (without indenting after)
 (define-key markdown-mode-map (kbd "C-y") 'yank)
