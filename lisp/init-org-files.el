82776423f3e50d242645bdaff9c20251bf8e49b4
Pedro Lambert <pedrolambert@gmail.com>
2016-08-16 15:48:00 -0400

    Move `org-mode` file navigation features to a separate file

---
+++ lisp/init-org-files.el
@@ -0,0 +1,33 @@
+(require 'f)
+(require 'projectile)
+(require 'helm)
+
+(defun custom/org-get-filenames ()
+  (when (file-directory-p custom/org-dir)
+    (--> custom/org-dir
+         (f-entries it (lambda (file) (string-match-p "\\.org$" file)) t)
+         (--map (f-base it) it)
+         (sort it 'string-lessp))))
+
+(defun custom/org-file-path (name)
+  (let ((org-file (format "%s.org" name)))
+    (f-join custom/org-dir org-file)))
+
+(defun custom/org-open-project-file (&optional name)
+  (interactive)
+  (let ((filename (or name (projectile-project-name))))
+    (find-file (custom/org-file-path filename))))
+
+;; create a helm buffer displaying all my .org files
+(defun custom/helm-org-files ()
+  "Display all files under `custom/org-dir` inside a helm buffer."
+  (interactive)
+  (helm :sources (custom/helm-org-files-source)
+        :buffer "*My Org Files*"))
+
+(defun custom/helm-org-files-source ()
+  (helm-build-sync-source "My Org Files"
+    :candidates (custom/org-get-filenames)
+    :action '(("Open file" . custom/org-open-project-file))))
+
+(provide 'init-org-files)
