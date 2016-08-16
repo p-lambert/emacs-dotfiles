c14a89df72628837b76d5af3e75fa532a5750640
Pedro Lambert <pedrolambert@gmail.com>
2016-08-16 12:53:46 -0400

    Fix `org-mode` identation level of code blocks

--- lisp/init-org-code.el
+++ lisp/init-org-code.el
@@ -3,8 +3,9 @@
 (defvar custom/org-langs '(ruby sh))
 
 (setq org-src-fontify-natively t
-      org-src-preserve-indentation t
-      org-src-tab-acts-natively t)
+      org-src-preserve-indentation nil
+      org-src-tab-acts-natively t
+      org-edit-src-content-indentation 0)
 
 ;; don't prompt for code evaluation
 (setq org-confirm-babel-evaluate nil)
