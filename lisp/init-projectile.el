aec4757bafd0aa6e57b7d9cc329c4550afa60576
Pedro Lambert <pedrolambert@gmail.com>
2016-10-19 01:26:18 -0400

    Add fallback for `custom/projectile-eshell`

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -21,13 +21,15 @@
 (defun custom/projectile-eshell ()
   "Open an eshell buffer at project's root directory."
   (interactive)
-  (let* ((shell-dir (projectile-project-root))
-         (shell-title (format "*eshell [%s]*" (projectile-project-name)))
-         (cmd (format "cd %s; clear" shell-dir))
-         (buffer (or (get-buffer shell-title)
-                     (custom/run-eshell-command shell-title cmd))))
-    (other-window 1)
-    (switch-to-buffer buffer)))
+  (if (projectile-project-p)
+      (let* ((shell-dir (projectile-project-root))
+             (shell-title (format "*eshell [%s]*" (projectile-project-name)))
+             (cmd (format "cd %s; clear" shell-dir))
+             (buffer (or (get-buffer shell-title)
+                         (custom/run-eshell-command shell-title cmd))))
+        (other-window 1)
+        (switch-to-buffer buffer))
+    (eshell)))
 
 ;; overwrite original `projectile-completing-read` with cleaner helm buffer
 (defun projectile-completing-read (prompt choices &optional initial-input)
