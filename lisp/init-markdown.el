031a0106de61058cc9e031d874082060379b2c50
Pedro Lambert <pedrolambert@gmail.com>
2017-08-10 15:28:23 -0400

    Remove `livedown` for now

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -1,5 +1,4 @@
 (require 'markdown-mode)
-(require 'livedown)
 
 ;; disable autoindent after pressing RET
 (setq markdown-indent-on-enter nil)
@@ -8,6 +7,5 @@
 
 ;; use vanilla yank function (without indenting after)
 (define-key markdown-mode-map (kbd "C-y") 'yank)
-(define-key markdown-mode-map (kbd "C-c C-c") 'livedown:preview)
 
 (provide 'init-markdown)
