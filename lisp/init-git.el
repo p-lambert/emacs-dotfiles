64e9dceed33c5625d30c22158637ab43319c4226
Pedro Lambert <pedrolambert@gmail.com>
2017-09-07 16:05:43 -0400

    Add custom git-branch switcher

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -1,9 +1,10 @@
 (require 'magit)
 (require 'git-timemachine)
+(require 'helm)
+(require 's)
 
 (global-set-key (kbd "C-c g g") 'magit-status)
 (global-set-key (kbd "C-c g c") 'magit-checkout)
-(global-set-key (kbd "C-c g b") 'magit-show-refs-popup)
 (global-set-key (kbd "C-c g f") 'magit-log-buffer-file)
 (global-set-key (kbd "C-c g t") 'git-timemachine)
 (global-set-key (kbd "C-c g l") 'vc-annotate)
@@ -21,6 +22,24 @@
     (kill-new (shell-command-to-string cmd))
     (message "Changelog copied to kill-ring.")))
 
+(defun custom/git-branches ()
+  (projectile-with-default-dir (projectile-project-root)
+    (--> (shell-command-to-string "/usr/local/bin/git branch")
+         (s-split "\n" it t)
+         (--map (substring it 2) it))))
+
+(defun custom/helm-git-branches ()
+  "Display a list of git branches"
+  (interactive)
+  (helm :sources (custom/helm-git-branches-source)
+        :buffer "*Git Branches*"))
+
+(defun custom/helm-git-branches-source ()
+  (helm-build-sync-source (format "Git Branches [%s]" (projectile-project-name))
+    :candidates (custom/git-branches)
+    :action '(("Switch to branch" . magit-checkout))))
+
 (global-set-key (kbd "C-c g m") 'custom/branch-changelog)
+(global-set-key (kbd "C-c g b") 'custom/helm-git-branches)
 
 (provide 'init-git)
