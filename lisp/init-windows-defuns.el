e71de15c9b7cd8546ff211f9e4717c2e572dbe63
Pedro Lambert <pedrolambert@gmail.com>
2017-11-03 11:57:52 -0400

    Add `custom/kill-other-buffers`

--- lisp/init-windows-defuns.el
+++ lisp/init-windows-defuns.el
@@ -55,4 +55,14 @@
         (custom/hsplit-last-buffer)
       (custom/vsplit-last-buffer))))
 
+;; https://github.com/bbatsov/crux/
+(defun custom/kill-other-buffers ()
+  "Kill all buffers but the current one.
+Doesn't mess with special buffers."
+  (interactive)
+  (when (y-or-n-p "Are you sure you want to kill all buffers but the current one? ")
+    (seq-each
+     #'kill-buffer
+     (delete (current-buffer) (seq-filter #'buffer-file-name (buffer-list))))))
+
 (provide 'init-windows-defuns)
