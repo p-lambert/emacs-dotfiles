87ef28211744ba4f02c666fac7d08655dbaaba67
Pedro Lambert <pedrolambert@gmail.com>
2018-02-10 17:16:04 -0500

    Add `s-o` keybinding for alternating between buffers

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -35,6 +35,7 @@
 ;; window and buffer manipulation
 (global-set-key (kbd "M-o") 'other-window)
 (global-set-key (kbd "M-0") 'delete-window)
+(global-set-key (kbd "s-o") (lambda () (interactive ) (switch-to-buffer nil nil 't)))
 (global-set-key (kbd "M-k") 'kill-buffer)
 (global-set-key (kbd "M-1") 'delete-other-windows)
 (global-set-key (kbd "M-2") 'split-window-vertically)
