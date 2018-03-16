efd50c8f074b1b3ebb5cffd348ce23cd00de5a49
Pedro Lambert <pedrolambert@gmail.com>
2018-03-16 11:07:01 -0400

    Improve search function

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -4,13 +4,20 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "s-f") 'helm-do-ag)
+(global-set-key (kbd "s-f") 'custom/helm-interactive-search)
 
 (setq helm-quick-update t
       helm-buffers-fuzzy-matching t
       helm-buffer-max-length 45
       helm-buffer-details-flag nil
       helm-display-header-line nil)
+
+(defun custom/helm-interactive-search (this-file-only-p)
+  (interactive "P")
+  (if this-file-only-p
+      (let ((current-prefix-arg nil))
+        (helm-do-ag-this-file))
+    (helm-do-ag)))
 
 ;; disable helm mode-line
 (with-eval-after-load 'helm
