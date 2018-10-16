bdd94cf190bea0cdd3290694531034578a2c59d9
Pedro Lambert <pedrolambert@gmail.com>
2018-10-16 15:09:23 -0400

    Add function for referencing code line on Github
    Shamelessly copied from @vlad-mh

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -42,7 +42,24 @@
     :action '(("Switch to branch" .
                (lambda (name) (magit-checkout (substring name 2)))))))
 
+(defun custom/github-copy-shareable-url ()
+  "Copies to kill-ring a shareable GitHub URL for the line the
+point is currently at."
+  (interactive)
+  (-when-let* ((branch (magit-get-current-branch))
+              (repo (magit-gh-pulls-guess-repo))
+              (fpath (magit-current-file))
+              (user (car repo))
+              (repo-name (cdr repo))
+              (urlpath (concat "/" user "/" repo-name "/blob/" branch "/" fpath))
+              (line (line-number-at-pos))
+              (urlattr (concat "L" (number-to-string line)))
+              (url (url-parse-make-urlobj
+                    "https" nil nil "github.com" nil urlpath urlattr nil t)))
+    (message (kill-new (url-recreate-url url)))))
+
 (global-set-key (kbd "C-c g m") 'custom/branch-changelog)
 (global-set-key (kbd "C-c g b") 'custom/helm-git-branches)
+(global-set-key (kbd "C-c g l") 'custom/github-copy-shareable-url)
 
 (provide 'init-git)
