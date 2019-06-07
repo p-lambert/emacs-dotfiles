222c1aa9d423faacea6e33de22c29b9698896bdc
Pedro Lambert <pedrolambert@gmail.com>
2019-06-07 19:02:09 -0400

    Improve projectile/elscreen integration

--- lisp/init-elscreen.el
+++ lisp/init-elscreen.el
@@ -8,17 +8,11 @@
 ;; custom keybindings defined in init-keybindings.el
 
 (defun custom/elscreen-mode-line ()
-  (let ((screen-list (sort (elscreen-get-screen-list) '<)))
+  (let ((screen-list (sort (elscreen-get-screen-list) '<))
+        (screen-name (elscreen-get-screen-nickname (elscreen-get-current-screen))))
     (format "%d/%d"
             (1+ (-elem-index (elscreen-get-current-screen) screen-list))
             (length screen-list))))
-
-(defun custom/elscreen-display ()
-  (interactive)
-  (let ((screen-list (sort (elscreen-get-screen-list) '<)))
-    (elscreen-message
-     (mapconcat 'custom/elscreen-name screen-list " | ")
-     5)))
 
 (defun custom/elscreen-name (screen)
   (let ((screen-name (alist-get screen (elscreen-get-screen-to-name-alist))))
@@ -27,15 +21,25 @@
       screen-name)))
 
 (defun custom/elscreen-with-display (fn &rest args)
-       (let ((result (apply fn args)))
-         (custom/elscreen-display)
-         result))
+  (let ((result (apply fn args)))
+    (elscreen-message (custom/elscreen-message) 5)
+    result))
+
+(defun custom/elscreen-message ()
+  (let ((screen-list (sort (elscreen-get-screen-list) '<)))
+    (mapconcat 'custom/elscreen-name screen-list " | ")))
+
+(defun custom/elscreen-from-nickname (nickname)
+  (car (rassoc (projectile-project-name)
+               (elscreen-get-screen-to-name-alist))))
 
 (dolist (fn '(elscreen-previous
               elscreen-next
               elscreen-create
-              elscreen-kill
-              elscreen-screen-nickname))
+              elscreen-kill))
   (advice-add fn :around #'custom/elscreen-with-display))
+
+(advice-add 'elscreen-screen-nickname :after
+            (lambda (&rest _) (message (custom/elscreen-message))))
 
 (provide 'init-elscreen)
--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -13,18 +13,18 @@
  projectile-switch-project-action 'custom/projectile-switch-action)
 
 (defun custom/projectile-switch-action ()
-  (projectile-dired)
-  (delete-other-windows))
+  (let ((screen (custom/elscreen-from-nickname (projectile-project-name))))
+    (if screen
+        (elscreen-goto screen)
+      (elscreen-clone)
+      (elscreen-screen-nickname (projectile-project-name))
+      (projectile-dired)
+      (delete-other-windows))))
 
 (setq helm-projectile-sources-list
       '(helm-source-projectile-buffers-list helm-source-projectile-files-list))
 
 (dolist (ignored custom/projectile-blacklist)
         (add-to-list 'projectile-globally-ignored-directories ignored))
-
-;; add `perspective` integration
-;; (require 'perspective)
-;; (persp-mode)
-;; (require 'persp-projectile)
 
 (provide 'init-projectile)
