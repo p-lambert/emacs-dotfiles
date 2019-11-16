dfd0867154c8cbee31ed3ba5904560f29a12d131
Pedro Lambert <pedrolambert@gmail.com>
2019-11-16 14:05:21 -0500

    Fix broken babel configuration

--- lisp/init-org-code.el
+++ lisp/init-org-code.el
@@ -1,7 +1,5 @@
 (require 'org-src)
 
-(defvar custom/org-langs '(ruby sh))
-
 (setq org-src-fontify-natively t
       org-src-preserve-indentation nil
       org-src-tab-acts-natively t
@@ -9,10 +7,5 @@
 
 ;; don't prompt for code evaluation
 (setq org-confirm-babel-evaluate nil)
-
-;; load org-babel support for specified languages
-(->> custom/org-langs
-     (--map (cons it t))
-     (org-babel-do-load-languages 'org-babel-load-languages))
 
 (provide 'init-org-code)
