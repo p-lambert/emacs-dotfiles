5fea25ed174456006b3afd4fc85a96b8f998a288
Pedro Lambert <pedrolambert@gmail.com>
2019-11-16 12:22:57 -0500

    Configure `C` indentation

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -1,8 +1,10 @@
 (require 'cc-mode)
 
-(add-hook 'c-mode-common-hook 'google-set-c-style)
+;; (add-hook 'c-mode-common-hook 'google-set-c-style)
 
 (c-set-offset 'innamespace 0)
+
+(setq-default c-basic-offset 4)
 
 (define-key c-mode-base-map (kbd "C-c , ,")
   (lambda () (interactive) (ff-find-other-file t)))
